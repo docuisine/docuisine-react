80be0afa9fed884670c52d967526f4535af2dcbd
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-12-25 18:00:22 +0800

    refactor: simplify NavBar component by using a sections array

--- src/components/custom/navbar.tsx
+++ src/components/custom/navbar.tsx
@@ -3,17 +3,20 @@ import { UtensilsCrossedIcon, BookTextIcon, ListTodo } from "lucide-react";
 
 const NavBar = () => {
   const iconSize = "1.2em";
+
+  const sections = [
+    { title: "Recipes", icon: <UtensilsCrossedIcon size={iconSize} /> },
+    { title: "Cookbooks", icon: <BookTextIcon size={iconSize} /> },
+    { title: "Shopping List", icon: <ListTodo size={iconSize} /> },
+  ];
+
   return (
     <div className="w-full flex bg-amber-50">
-      <NavBarBtn title="Recipes">
-        <UtensilsCrossedIcon size={iconSize} />
-      </NavBarBtn>
-      <NavBarBtn title="Cookbooks">
-        <BookTextIcon size={iconSize} />
-      </NavBarBtn>
-      <NavBarBtn title="Shopping List">
-        <ListTodo size={iconSize} />
-      </NavBarBtn>
+      {sections.map((section) => (
+        <NavBarBtn key={section.title} title={section.title}>
+          {section.icon}
+        </NavBarBtn>
+      ))}
     </div>
   );
 };
