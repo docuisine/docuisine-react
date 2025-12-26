d02202bb0a6cb3a6275b83d5674ae98464f0f056
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-12-26 19:59:34 +0800

    feat: update CuisineCarousel to use dynamic backend URL

--- src/components/custom/cuisine-carousel.tsx
+++ src/components/custom/cuisine-carousel.tsx
@@ -3,29 +3,36 @@ import CuisineCard from "@/components/custom/cuisinecard";
 import { useEffect, useState } from "react";
 import type { Category } from "@/lib/interface";
 import Axios from "axios";
+import { urlJoin } from "@/lib/utils";
 
 
 const CuisineCarousel = () => {
   const [cuisines, setCuisines] = useState<Category[]>([]);
 
   const getCategories = async () => {
-    const response = await Axios.get('http://localhost:7000/categories');
+    const response = await Axios.get(
+      urlJoin(import.meta.env.VITE_BACKEND_URL, "categories")
+    );
     setCuisines(response.data);
   };
   useEffect(() => {
     getCategories();
   }, []);
 
-  return (
-    <ScrollArea className="w-full h-fit pb-4 rounded-md whitespace-nowrap">
-      <div className="flex w-max space-x-4">
-        {cuisines.map((cuisine) => (
-          <CuisineCard category={cuisine} />
-        ))}
-      </div>
-      <ScrollBar orientation="horizontal" />
-    </ScrollArea>
-  );
+  if (cuisines) {
+    return (
+      <ScrollArea className="w-full h-fit pb-4 rounded-md whitespace-nowrap">
+        <div className="flex w-max space-x-4">
+          {cuisines.map((cuisine) => (
+            <CuisineCard category={cuisine} />
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
 };
 
 export default CuisineCarousel;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function urlJoin(...parts: string[]) {
+  return parts
+    .map((part, index) => {
+      if (index === 0) {
+        return part.replace(/\/+$/g, ""); // Remove trailing slashes from the first part
+      } else {
+        return part.replace(/^\/+|\/+$/g, ""); // Remove leading and trailing slashes from other parts
+      }
+    })
+    .filter((part) => part.length > 0) // Remove empty parts
+    .join("/");
+}
