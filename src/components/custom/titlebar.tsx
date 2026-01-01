4353ddba90cf6078bc5909c7ad69b3cdffd19892
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-01-01 11:10:32 +0800

    feat: display app version in TitleBar component

--- src/components/custom/titlebar.tsx
+++ src/components/custom/titlebar.tsx
@@ -5,12 +5,16 @@ import { useAuth } from "@/lib/useAuth";
 
 const TitleBar = () => {
   const { user } = useAuth();
+  const appVersion = import.meta.env.VITE_APP_VERSION;
   return (
     <div className="w-full flex justify-between">
       <Link to="/" className="flex items-center">
         <HorizontalLogo />
       </Link>
       <div className="flex flex-row gap-4 align-middle justify-center items-center">
+        {appVersion && (
+          <span className="text-xs text-muted-foreground">v{appVersion.slice(0, 7)}</span>
+        )}
         <Usercard username={user || "Guest"} />
       </div>
     </div>
