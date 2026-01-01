3084ba3197083be937965f230f9c3068d6b9c94c
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-01-01 13:06:36 +0800

    feat: add recipe description display on hover

--- src/components/custom/recipes/recipecard.tsx
+++ src/components/custom/recipes/recipecard.tsx
@@ -10,22 +10,35 @@ export interface Recipe {
   imageSrc: string;
   timeTaken: number; // in minutes
   servings: number;
+  description?: string;
 }
 
 const RecipeCard = ({ recipe }: { recipe: Recipe }) => {
   const normalizedPath = recipe.title.toLowerCase().replace(/\s+/g, "-");
   return (
     <Link to={`/recipe/${normalizedPath}`} className="w-fit">
-      <Card className="max-w-sm rounded-md p-0 overflow-hidden gap-0 w-80 max-h-54">
-        <CardContent className="px-0 overflow-hidden">
+      <Card className="max-w-sm rounded-md p-0 overflow-hidden gap-0 w-60 max-h-54">
+        <CardContent className="px-0 overflow-hidden h-28 relative w-full group">
+          {recipe.description && (
+            <div
+              className="absolute inset-0 bg-accent/90 p-2 h-28 opacity-0 translate-y-2 transition-all
+            duration-200 ease-in-out group-hover:opacity-100 group-hover:translate-y-0"
+            >
+              <p className="text-sm text-start text-foreground leading-relaxed whitespace-normal line-clamp-4">
+                {recipe.description}
+              </p>
+            </div>
+          )}
           <img
             src={recipe.imageSrc}
             alt={recipe.title}
             className="object-cover"
           />
         </CardContent>
-        <CardFooter className="flex flex-col items-start gap-4 p-4">
-          <CardTitle>{recipe.title}</CardTitle>
+        <CardFooter className="flex flex-col items-start gap-4 py-2 px-2 w-full">
+          <CardTitle className="text-start leading-relaxed truncate w-full">
+            {recipe.title}
+          </CardTitle>
           <Badge variant="outline">
             <HourglassIcon /> {recipe.timeTaken} mins
           </Badge>
