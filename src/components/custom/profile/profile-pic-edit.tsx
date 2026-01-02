44a1dd4a9f02f01b478bcc632bda1e2278071579
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-01-02 22:08:30 +0800

    refactor: replaced button with a simple div

--- src/components/custom/profile/profile-pic-edit.tsx
+++ src/components/custom/profile/profile-pic-edit.tsx
@@ -1,35 +1,16 @@
-import { PencilIcon, ImageUpIcon, XIcon } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+import { PencilIcon } from "lucide-react";
+import UserImageFileUpload from "./picture-upload";
 
 const EditProfilePic = () => {
   return (
     <div className="relative">
       <div className="absolute left-0 bottom-4 block">
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button variant="outline" size="icon" className="w-fit p-4">
-              <PencilIcon />
-              Edit
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-fit p-2">
-            <div className="grid w-full grid-rows-2 gap-2">
-              <Button variant="ghost" size="sm" className="justify-start">
-                <ImageUpIcon /> Upload new image
-              </Button>
-              <Button variant="ghost" size="sm" className="justify-start">
-                <XIcon />
-                Remove image
-              </Button>
-            </div>
-          </PopoverContent>
-        </Popover>
+        <UserImageFileUpload>
+          <div className="flex gap-2 items-center border px-2 py-1 rounded-sm cursor-pointer bg-background text-sm shadow-xs hover:bg-accent transition-colors transition-duration-400">
+            <PencilIcon size="16" className="text-muted-foreground" />
+            Edit
+          </div>
+        </UserImageFileUpload>
       </div>
     </div>
   );
