8cc354e00331441010666c3b098510008f7457b3
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-01-02 22:33:44 +0800

    fix: adjust layout and styling in EditProfilePic component

--- src/components/custom/profile/profile-pic-edit.tsx
+++ src/components/custom/profile/profile-pic-edit.tsx
@@ -4,9 +4,9 @@ import UserImageFileUpload from "./picture-upload";
 const EditProfilePic = () => {
   return (
     <div className="relative">
-      <div className="absolute left-0 bottom-4 block">
+      <div className="absolute left-0 bottom-8 block">
         <UserImageFileUpload>
-          <div className="flex gap-2 items-center border px-2 py-1 rounded-sm cursor-pointer bg-background text-sm shadow-xs hover:bg-accent transition-colors transition-duration-400">
+          <div className="flex gap-2 items-center border px-3 text-sm font-medium py-1.5 rounded-sm cursor-pointer bg-background shadow-xs hover:bg-accent transition-colors transition-duration-400">
             <PencilIcon size="16" className="text-muted-foreground" />
             Edit
           </div>
