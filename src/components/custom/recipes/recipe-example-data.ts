3fd7d5a575bb8794cc9ed9f581e399777d1ef599
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-01-13 14:57:30 +0800

    🔧 update(recipes): move exampleRecipes to a separate file for better organization

---
+++ src/components/custom/recipes/recipe-example-data.ts
@@ -0,0 +1,43 @@
+import exampleRecipeJpg from "@/assets/example-recipe.jpg";
+
+const exampleRecipes = [
+  {
+    title: "Spaghetti Carbonara",
+    imageSrc: exampleRecipeJpg,
+    timeTaken: 25,
+    servings: 4,
+    rating: 4.5,
+    description:
+      "Classic Italian pasta dish made with eggs, cheese, pancetta, and pepper.",
+  },
+  {
+    title: "Chicken Tikka Masala",
+    imageSrc: exampleRecipeJpg,
+    timeTaken: 40,
+    servings: 4,
+    rating: 4.5,
+  },
+  {
+    title: "Chicken Tikka Masala",
+    imageSrc: exampleRecipeJpg,
+    timeTaken: 40,
+    servings: 4,
+    rating: 4.5,
+  },
+  {
+    title: "Chicken Tikka Masala",
+    imageSrc: exampleRecipeJpg,
+    timeTaken: 40,
+    servings: 4,
+    rating: 4.5,
+  },
+  {
+    title: "Chicken Tikka Masala",
+    imageSrc: exampleRecipeJpg,
+    timeTaken: 40,
+    servings: 4,
+    rating: 4.5,
+  },
+];
+
+export default exampleRecipes;
--- src/components/custom/recipes/recipesxplorer.tsx
+++ src/components/custom/recipes/recipesxplorer.tsx
@@ -1,46 +1,7 @@
 import CuisineCarousel from "@/components/custom/cuisines/cuisine-carousel";
 import Searchbar from "@/components/custom/searchbar";
 import RecipeCarousel from "./recipe-carousel";
-import exampleRecipeJpg from "@/assets/example-recipe.jpg";
-
-const exampleRecipes = [
-  {
-    title: "Spaghetti Carbonara",
-    imageSrc: exampleRecipeJpg,
-    timeTaken: 25,
-    servings: 4,
-    rating: 4.5,
-    description: "Classic Italian pasta dish made with eggs, cheese, pancetta, and pepper.",
-  },
-  {
-    title: "Chicken Tikka Masala",
-    imageSrc: exampleRecipeJpg,
-    timeTaken: 40,
-    servings: 4,
-    rating: 4.5,
-  },
-  {
-    title: "Chicken Tikka Masala",
-    imageSrc: exampleRecipeJpg,
-    timeTaken: 40,
-    servings: 4,
-    rating: 4.5,
-  },
-  {
-    title: "Chicken Tikka Masala",
-    imageSrc: exampleRecipeJpg,
-    timeTaken: 40,
-    servings: 4,
-    rating: 4.5,
-  },
-  {
-    title: "Chicken Tikka Masala",
-    imageSrc: exampleRecipeJpg,
-    timeTaken: 40,
-    servings: 4,
-    rating: 4.5,
-  },
-];
+import exampleRecipes from "./recipe-example-data";
 
 const carousels = [
   {
