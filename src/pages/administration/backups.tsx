3d887bf89a344808cf01809316468953669303eb
Chris Irag <iragca@pm.me>
2026-02-05 09:53:00 +0800

    📦 new: add backups UI

--- src/pages/administration/backups.tsx
+++ src/pages/administration/backups.tsx
@@ -3,13 +3,147 @@ import {
   MiniPageSection,
   MiniPageSectionContent,
 } from "@/components/custom/profile/common";
+import { RotateCwIcon, CloudUploadIcon, CirclePlayIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableRow,
+} from "@/components/ui/table";
+import { Field, FieldLabel, FieldContent } from "@/components/ui/field";
+import { Switch } from "@/components/ui/switch";
+import { useState } from "react";
+
+function RefreshBtn() {
+  return (
+    <Tooltip>
+      <TooltipTrigger>
+        <Button variant="ghost">
+          <RotateCwIcon size={20} />
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>Refresh backup list</TooltipContent>
+    </Tooltip>
+  );
+}
+
+function UploadBackupBtn() {
+  return (
+    <Tooltip>
+      <TooltipTrigger>
+        <Button variant="ghost">
+          <CloudUploadIcon size={20} />
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>Upload backup</TooltipContent>
+    </Tooltip>
+  );
+}
+
+function Header() {
+  return (
+    <div className="rounded-md flex items-center gap-2">
+      <span className="text-sm">Backup and restore your Docuisine data</span>
+      <RefreshBtn />
+      <UploadBackupBtn />
+    </div>
+  );
+}
+
+function BackupItem() {
+  return (
+    <TableRow>
+      <TableCell className="font-medium">INV001</TableCell>
+      <TableCell>Paid</TableCell>
+      <TableCell>Credit Card</TableCell>
+      <TableCell className="text-right">$250.00</TableCell>
+    </TableRow>
+  );
+}
+
+function BackupsTable() {
+  const items = [];
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableCaption className="border-t p-1 mt-0">
+          <Button
+            variant="ghost"
+            className="text-foreground font-semibold w-full"
+          >
+            <CirclePlayIcon size={20} /> Initialize new backup
+          </Button>
+        </TableCaption>
+        <TableBody>
+          {items.length === 0 ? (
+            <TableRow>
+              <TableCell
+                colSpan={4}
+                className="text-center py-4 text-muted-foreground"
+              >
+                No backups yet.
+              </TableCell>
+            </TableRow>
+          ) : (
+            items.map((item) => <BackupItem key={item.id} />)
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
+
+function BackupsOption({
+  label,
+  children,
+}: {
+  label: string;
+  children?: React.ReactNode;
+}) {
+  const [enabled, setEnabled] = useState(false);
+
+  return (
+    <Field>
+      <div className="flex flex-row gap-4">
+        <Switch checked={enabled} onCheckedChange={setEnabled} />
+        <FieldLabel>{label}</FieldLabel>
+      </div>
+      <FieldContent
+        className={`${enabled ? "block" : "hidden"} transition transition-transform`}
+      >
+        {children}
+      </FieldContent>
+    </Field>
+  );
+}
 
 export default function BackupsPage() {
   return (
     <MiniPage>
       <MiniPageSection title="Backups">
-        <MiniPageSectionContent>
-          <p>Here you can manage backups of the application.</p>
+        <MiniPageSectionContent className="flex flex-col gap-4">
+          <Header />
+          <BackupsTable />
+        </MiniPageSectionContent>
+      </MiniPageSection>
+      <MiniPageSection title="Options">
+        <MiniPageSectionContent className="flex flex-col gap-4">
+          <BackupsOption label="Enable automatic backups">
+            <div className="flex flex-col gap-2">
+              Automatic backups is enabled.
+            </div>
+          </BackupsOption>
+          <BackupsOption label="Store backups in S3 storage">
+            <div className="flex flex-col gap-2">S3 storage is enabled.</div>
+          </BackupsOption>
+          <Button className="self-end max-w-xs mt-2">Save changes</Button>
         </MiniPageSectionContent>
       </MiniPageSection>
     </MiniPage>
